f6da69bea6402c237d6483359f4583eb84c1e651
Michael Ratliff <email@michaelratliff.com>
2013-04-11 20:46:24 -0400

    cleanup

--- src/dvr.js
+++ src/dvr.js
@@ -1,16 +1,9 @@
-#!/usr/bin/env node
-
 var _=require('underscore');
 var net=require('net');
-var stream=require('stream');
 
-var fs=require('fs');
-var util=require('util');
+var H264Stripper=require('./h264Stripper.js');
 
 var REQUEST_SIZE=507;
-var SCAN_SIZE=1024;
-var SCAN_REMAINDER=6;
-var STAGE1_HEAD=16;
 
 var DVR=function(options) {
 	this.options=options;
@@ -120,77 +113,8 @@ var RecordingRequest=function(recording) {
 	this.buffer.write(recording,51,null,'ascii');
 };
 
-_.extend(RecordingRequest.prototype,Request.prototype);
+_.extend(RecordingRequest.prototype,Request.prototype);	
 
-var H264Stripper=function(inStream,options) {
-	this.inStream=inStream;
-	
-	inStream.on('end',function() {
-		this.push(null);
-	});
-	
-	inStream.on('readable',_.bind(this.readIn,this));
-	
-	this.stage=0;
-	this.pushable=true;
-	this.readSize=SCAN_SIZE;
-	
-	stream.Readable.call(this,options);
-};
-
-H264Stripper.prototype = Object.create(stream.Readable.prototype, { constructor: { value: H264Stripper }});
-
-H264Stripper.prototype.readIn=function() {
-		var buf;
-		
-		//console.log('Will read ' + this.readSize + ' bytes');
-		while((this.stage!=3 || this.pushable) && (buf=this.inStream.read(this.readSize))) {
-			//console.log('Read ' + buf.length + ' bytes for stage: ' + this.stage);
-			//console.dir(buf);
-			//if (buf.length>10) console.dir(buf.slice(buf.length-10));
-			switch(this.stage) {
-			case 0:
-				var i;
-				for(i=0;this.stage==0 && i<=buf.length-SCAN_REMAINDER;i++) {
-					if (buf[i]==0x64 && buf[i+1]==0x63 && buf[i+2]==0x48 && buf[i+3]==0x32 && buf[i+4]==0x36 && buf[i+5]==0x34) {
-						this.stage=1;
-						this.readSize=STAGE1_HEAD;
-						i--;
-					}
-				}
-				
-
-				this.inStream.unshift(buf.slice(i));			
-				break;
-			case 1:
-				this.stage=2;
-				this.nextChunk=buf.readUInt32LE(6);
-				this.readSize=buf.readUInt32LE(10) + 6;
-				break;
-			case 2:
-				this.stage=3;
-				this.readSize=this.nextChunk;
-				break;
-			case 3:
-				this.stage=0;
-				this.readSize=SCAN_SIZE;
-				this.pushable=this.push(buf);
-				break;
-			}
-		}
-	};
-	
-H264Stripper.prototype._read=function() {
-		if(!this.pushable) {
-			this.pushable=true;
-			this.readIn();
-		}
-	};	
-
-var dvr=new DVR({host: '10.0.0.21', port: 9000});
-//dvr.getRecording('ch00000000000001-130405-235004-235959-10p00001000000400.nvr').pipe(fs.createWriteStream('test2.h264'));
-dvr.getChannel(1).pipe(process.stdout);
-//dvr.getRecordingList(2013,4,10,console.dir);
 
 module.exports={
 	Request: Request,
---
+++ src/dvrTest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+var DVR=require('./dvr.js').DVR;
+
+var dvr=new DVR({host: '10.0.0.21', port: 9000});
+//dvr.getRecording('ch00000000000001-130405-235004-235959-10p00001000000400.nvr').pipe(fs.createWriteStream('test2.h264'));
+dvr.getChannel(1).pipe(process.stdout);
+//dvr.getRecordingList(2013,4,10,console.dir);
